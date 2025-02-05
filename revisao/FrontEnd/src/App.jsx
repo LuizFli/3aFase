8e8cf9588eeb9e3ba73ba053f6f24172bce75a8b
LuiFli <filipefln@gmail.com>
2025-02-04 22:03:26 -0300

    att

---
+++ revisao/FrontEnd/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  
+
+  return (
+    <div className='div1'>
+
+        <Header/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ revisao/FrontEnd/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='divHeader'>
+
+        <h1>Minha Page</h1>
+      
+    </div>
+  )
+}
+
+export default Header
